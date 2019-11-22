45d0eb4e4e6314e006ef673c84bdc803ae49cbe3
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2019-11-21 21:43:23 -0500

    Add files via upload

---
+++ React JS/build/precache-manifest.ed3f033d648ddbae226f4d1c1737a9f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca726b146ab979477046c2e03fff76a",
+    "url": "/bhavikbhoir/react-profile.git/index.html"
+  },
+  {
+    "revision": "efe22507791d685e0898",
+    "url": "/bhavikbhoir/react-profile.git/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "bc87830dcc8bd28af34a",
+    "url": "/bhavikbhoir/react-profile.git/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "efe22507791d685e0898",
+    "url": "/bhavikbhoir/react-profile.git/static/js/2.d190848a.chunk.js"
+  },
+  {
+    "revision": "bc87830dcc8bd28af34a",
+    "url": "/bhavikbhoir/react-profile.git/static/js/main.dba81cfb.chunk.js"
+  },
+  {
+    "revision": "55098d365de098785219",
+    "url": "/bhavikbhoir/react-profile.git/static/js/runtime-main.f731326d.js"
+  },
+  {
+    "revision": "7a09a931319b2079c1fce04321ad6768",
+    "url": "/bhavikbhoir/react-profile.git/static/media/Wallpaper.7a09a931.jpg"
+  },
+  {
+    "revision": "005b6235919c52d2dc82128c65e8af52",
+    "url": "/bhavikbhoir/react-profile.git/static/media/logo.005b6235.jpg"
+  },
+  {
+    "revision": "48af9bcc087e91e1fd3187e336aa73f2",
+    "url": "/bhavikbhoir/react-profile.git/static/media/profile.48af9bcc.jpg"
+  }
+]);
---
+++ React JS/build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bhavikbhoir/react-profile.git/precache-manifest.ed3f033d648ddbae226f4d1c1737a9f4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bhavikbhoir/react-profile.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
